e953d8f9a2573c827d2bb6e57b6ef423c487f11b
Abhirupdatta9 <Abhirupdatta9@users.noreply.github.com>
2019-04-09 22:53:00 +0530

    heap modified

--- Heap and Priority Queue/heap.c
+++ Heap and Priority Queue/heap.c
@@ -7,7 +7,7 @@ void randomArray(int arr[], int n)
 	srand(time(0));
 	for(int i = 0; i<n; i++)
 	{
-		arr[i] = rand();
+		arr[i] = rand()%99;
 	}
 }
 
@@ -18,6 +18,10 @@ int left(int i)
 int right(int i)
 {
 	return (2*i)+2;
+}
+int parent(int i)
+{
+  return (i/2)-1;
 }
 void swap(int *a,int *b)
 {
@@ -116,11 +120,91 @@ void display(int arr[],int n)
 		printf("%d,",arr[i]);
 	}
 }
+void tabularCase()
+{
+	int n;
+	printf("\nS.no.\tn\tRandom Data\tData in Ascending\tData inDescending\n");
+	for(int a=1;a<=10;a++)
+	{
+		printf("%d\t",a);
+		scanf("%d",&n);
+		printf("\t\t");
+		int arr[n];
+		randomArray(arr,n);
+		clock_t t;
+
+		t=clock();
+		heapSortAsc(arr,n);
+        	t=clock()-t;
+		double time_taken=((double)t/CLOCKS_PER_SEC);
+		printf("%lf",time_taken);
+		printf("\t");
+
+		clock_t x=clock();
+		heapSortAsc(arr,n);
+       	x=clock()-x;
+		time_taken=((double)x/CLOCKS_PER_SEC);
+		printf("%lf\t",time_taken);
+
+		clock_t y=clock();
+		heapSortDesc(arr,n);
+		y=clock()-y;
+		time_taken=((double)x/CLOCKS_PER_SEC);
+		printf("%lf\n",time_taken);
+
+		printf("\n");
+	}
+}
+int heap_extract_max(int arr[],int n)
+{
+  if(n<1)
+  {
+    printf("Error\n");
+  }
+  else
+  {
+    int max;
+    max=arr[0];
+    arr[0]=arr[n-1];
+    n--;
+    max_heapify(arr,n,0);
+    return max;
+  }
+}
+void heap_increase_key(int arr[],int n,int i,int key)
+{
+  // if(key<=arr[i])
+  // {
+  //   printf("Error: new value is lesser than the old");
+  // }
+  // else
+  // {
+    arr[i]=key;
+    while(i>0 && arr[i]>arr[parent(i)] )
+    {
+      swap(&arr[i],&arr[parent(i)]);
+      i=parent(i);
+    }
+  // }
+
+}
+void heap_insert(int arr[],int n,int key)
+{
+  if(n<1)
+  {
+    printf("Error");
+  }
+  else
+  {
+    arr[n]=INT_MIN;
+    heap_increase_key(arr,n+1,n-1,key);
+  }
+
+}
 int main()
 {
 	int n, choice; char ans = 'y';
-	printf("Enter the number of elements?");
-	scanf("%d",&n);
+	int pos,key;
 
 	int arr[n];
 	clock_t start, end;
@@ -135,14 +219,17 @@ int main()
 		printf("\n 9. extract largest element");
 		printf("\n10. replace value at a node with a new node");
 		printf("\n11. insert a new element");
-		printf("\n12. delete an element");
+		// printf("\n12. delete an element");
 
 		printf("\n\nEnter choice :");
 		scanf("%d", &choice);
 		switch(choice)
 		{
 			case 0: exit(1);
-			case 1: randomArray(arr,n);
+			case 1:
+				printf("Enter the number of elements?");
+				scanf("%d",&n);
+				randomArray(arr,n);
 				   break;
 			case 2: display(arr,n);
 				   break;
@@ -150,13 +237,59 @@ int main()
 				   break;
 			case 4: heapSortDesc(arr,n);
 				   break;
-			case 5: heapSortAsc(arr,n);
+			case 5:
+			 	   randomArray(arr,n);
 				   start = clock();
 				   heapSortAsc(arr,n);
 				   end = clock();
 				   printf("\ntime: %lf", ((double)(start-end))/CLOCKS_PER_SEC);
 				   break;
+		   	case 6:
+					randomArray(arr,n);
+					heapSortAsc(arr,n);
+					start=clock();
+					heapSortAsc(arr,n);
+					end=clock();
+					printf("\ntime: %lf", ((double)(start-end))/CLOCKS_PER_SEC);
+  				   	break;
+	   		case 7:
+					randomArray(arr,n);
+					heapSortDesc(arr,n);
+					start=clock();
+					heapSortAsc(arr,n);
+					end=clock();
+					printf("\ntime: %lf", ((double)(start-end))/CLOCKS_PER_SEC);
+					break;
 
+			case 8:
+				tabularCase();
+				break;
+
+			case 9:
+				build_max_heap(arr,n);
+				printf("Extract heap max:-%d\n",heap_extract_max(arr,n));
+				printf("Array after extracting max\n");
+				display(arr,n-1);
+				break;
+
+			case 10:
+				build_max_heap(arr,n);
+				printf("\nFor replace key:-\n");
+				printf("Enter position and new value?");
+				scanf("%d%d",&pos,&key);
+				heap_increase_key(arr,n,pos,key);
+				printf("New array\n");
+				display(arr,n-1);
+				break;
+
+			case 11:
+				build_max_heap(arr,n);
+				printf("\nFor inserting a new value\n");
+				printf("Enter value\n");
+				scanf("%d",&key);
+				heap_insert(arr,n,key);
+				display(arr,n);
+				break;
 		}
 		printf("\nContinue?(y/n) ");
 		scanf(" %c", &ans);
--- Max-Min/max-min.c
+++ Max-Min/max-min.c
@@ -28,7 +28,7 @@ void display(int arr[],int n)
 		struct res result;
 		  max=arr[p];
 		  min=arr[p];
-		 
+
 		 for(int i=p+1;i<r;i++)
 		 {
 			if(arr[i]>max)
@@ -51,7 +51,7 @@ void display(int arr[],int n)
 		  result.minimum=arr[p];
 		 }
 		else if(p==r-1)
-		{  
+		{
 		   if(arr[p]>arr[r])
 		   {   result.maximum=arr[p];
 			  result.minimum=arr[r];
@@ -64,7 +64,7 @@ void display(int arr[],int n)
 		}
 		else{
 		  int q=(p+r)/2;
-		
+
 		 temp1=DIVIDE_MAXMIN( arr, p,q);
 		temp2=DIVIDE_MAXMIN( arr, q+1,r);
 		 if(temp1.maximum>temp2.maximum)
@@ -101,15 +101,15 @@ clock_t t;
 		t=clock();
        struct res result=ITERATIVE_MAXMIN(arr,0,n-1,max,min);
         t=clock()-t;
-	
+
 		double time_taken=((double)t/CLOCKS_PER_SEC);
 		printf("%f",time_taken);
 		printf("\t");
-		
+
 		clock_t x=clock();
 		result=DIVIDE_MAXMIN(arr,0,n-1);
        x=clock()-x;
-	
+
 		time_taken=((double)x/CLOCKS_PER_SEC);
 		printf("%f\n",time_taken);
 		printf("\n");
@@ -117,34 +117,34 @@ clock_t t;
 }
 
 void main()
-{ 
+{
    int n;
   int ch;
   printf("\n enter the no of terms\n");
          scanf("%d",&n);
 int max=0;
 int min=0;
-      
+
       int arr[n];
    printf("\n\t options available\n\t 0.quit\n\t1.generate random numbers and store in an array\n\t2.display array\n\t3.max and min with iterative\n\t4.max and min with dnc\n\t5.compare");
   do{
-     
+
       printf("\n enter your choice\n");
       scanf("%d",&ch);
-      
+
    switch(ch)
    {
 
     case 0:
-      { 
+      {
         exit(0);
         break;
       }
   case 1:
-  
+
       {  generate(arr,n);
-      	
-        
+
+
         break;
       }
    case 2:
